baf9ac9ae858714d68db0bb1c08923057fc1fb21
brendan <brendangooch@gmail.com>
2024-12-07 14:15:25 +0000

    v1.1.0     added empty CardGameSettings class that all games extend; this is passed to all core game objects on init()

---
+++ src/card-game-settings.ts
@@ -0,0 +1,5 @@
+/**
+ * a blank settings class that all games extend and then add their own settings components as required
+ */
+
+export abstract class CardGameSettings { }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
 export type tGameDifficulty = 'beginner' | 'easy' | 'normal' | 'hard' | 'expert';
 export type tGameMode = 'demo' | 'client' | 'server';
 
+export { CardGameSettings } from "./card-game-settings.js";
 export { DifficultySetting } from "./difficulty-setting.js";
 export { ModeSetting } from "./mode-setting.js";
 export { NumericSetting } from "./numeric-setting.js";
